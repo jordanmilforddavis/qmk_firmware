a5948e3af1b17623b66622b7b8d7ec066a7be2c2
Jordan Davis <jordanmilforddavis@gmail.com>
2020-11-28 23:33:06 -0800

    add jordanmilforddavis ergodox_ez config from oryx

---
+++ keyboards/ergodox_ez/keymaps/jordanmilforddavis/rules.mk
@@ -0,0 +1,8 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+LTO_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = IS31FL3731
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SRC = matrix.c
