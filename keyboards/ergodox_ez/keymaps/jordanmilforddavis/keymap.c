855ce6f956bb398ddb5142abc3bc7af0c9a48a8e
Jordan Davis <jordanmilforddavis@gmail.com>
2020-11-30 11:05:42 -0800

    update jordanmilforddavis ergodox_ez config

--- keyboards/ergodox_ez/keymaps/jordanmilforddavis/keymap.c
+++ keyboards/ergodox_ez/keymaps/jordanmilforddavis/keymap.c
@@ -21,13 +21,39 @@ enum custom_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
 };
 
+enum unicode_names {
+  IDENTICAL,
+  INTERSECTION,
+  UNION,
+  SUBSET,
+  SUPERSET,
+  TILDE,
+  MACRON,
+  EMPTYSET,
+  AND,
+  OR,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [IDENTICAL]     = 0x2261,  // ≡
+    [INTERSECTION]  = 0x2229,  // ∩
+    [UNION]         = 0x222a,  // ∪
+    [SUBSET]        = 0x2282,  // ⊂
+    [SUPERSET]      = 0x2283,  // ⊃
+    [TILDE]         = 0x223C,  // ∼
+    [MACRON]        = 0xAF  ,  // ¯
+    [EMPTYSET]      = 0x2205,  // ∅
+    [AND]           = 0x2227,  // ∧
+    [OR]            = 0x2228,  // ∨
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_AUDIO_MUTE,                                  KC_CAPSLOCK,    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSLASH,
     KC_QUOTE,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRACKET,                                    KC_RBRACKET,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_EQUAL,
     KC_ESCAPE,      LSFT_T(KC_A),   LCTL_T(KC_S),   LALT_T(KC_D),   LGUI_T(KC_F),   KC_G,                                                                           KC_H,           LGUI_T(KC_J),   LALT_T(KC_K),   LCTL_T(KC_L),   LSFT_T(KC_SCOLON),KC_MINUS,
     KC_COLN,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_LCBR,                                        KC_RCBR,        KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT,
-    KC_F4,          KC_MAC_COPY,    KC_MAC_PASTE,   LCTL(KC_LEFT),  LCTL(KC_RIGHT),                                                                                                 LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_F4,          KC_MAC_COPY,    KC_MAC_PASTE,   LCTL(KC_LEFT),  LCTL(KC_RIGHT),                                                                                                 LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, LT(3, KC_SLASH),
                                                                                                     RGB_SAD,        RGB_SAI,        RGB_MOD,        RGB_SLD,
                                                                                                                     RGB_VAI,        RGB_HUI,
                                                                                     LT(1,KC_SPACE), LT(2,KC_TAB),   RGB_VAD,        RGB_HUD,        KC_BSPACE,      KC_ENTER
@@ -42,8 +68,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
+  [3] = LAYOUT_ergodox_pretty(
+    KC_TRANSPARENT, X(IDENTICAL),   X(INTERSECTION),X(UNION),       X(SUBSET),      X(SUPERSET),    KC_TRANSPARENT,                                 KC_TRANSPARENT, S(KC_4),        S(KC_5),        X(TILDE),       X(MACRON),      X(EMPTYSET),    KC_F11,
+    KC_TRANSPARENT, X(AND),         X(OR),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F12,
+    KC_TRANSPARENT, KC_LSHIFT,      KC_LCTRL,       KC_LALT,        KC_LGUI,        KC_TRANSPARENT,                                                                 KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT,
+                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+  ),
 };
-
 
 extern bool g_suspend_state;
 extern rgb_config_t rgb_matrix_config;
@@ -52,38 +87,17 @@ void keyboard_post_init_user(void) {
   rgb_matrix_enable();
 }
 
-const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
-    [1] = { {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {0,0,255}, {0,0,255}, {0,0,255}, {0,0,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255}, {222,128,255} },
-
-    [2] = { {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242}, {116,237,242} },
-
-};
-
-void set_layer_color(int layer) {
-  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    HSV hsv = {
-      .h = pgm_read_byte(&ledmap[layer][i][0]),
-      .s = pgm_read_byte(&ledmap[layer][i][1]),
-      .v = pgm_read_byte(&ledmap[layer][i][2]),
-    };
-    if (!hsv.h && !hsv.s && !hsv.v) {
-        rgb_matrix_set_color( i, 0, 0, 0 );
-    } else {
-        RGB rgb = hsv_to_rgb( hsv );
-        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
-    }
-  }
-}
-
 void rgb_matrix_indicators_user(void) {
   if (g_suspend_state || keyboard_config.disable_layer_led) { return; }
   switch (biton32(layer_state)) {
     case 1:
-      set_layer_color(1);
+      rgb_matrix_set_color_all(108, 110, 160);
       break;
     case 2:
-      set_layer_color(2);
+      rgb_matrix_set_color_all(254, 215, 102);
+      break;
+    case 3:
+      rgb_matrix_set_color_all(255, 20, 80);
       break;
    default:
     if (rgb_matrix_get_flags() == LED_FLAG_NONE)
@@ -102,44 +116,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
-
-uint32_t layer_state_set_user(uint32_t state) {
-
-  uint8_t layer = biton32(state);
-
-  ergodox_board_led_off();
-  ergodox_right_led_1_off();
-  ergodox_right_led_2_off();
-  ergodox_right_led_3_off();
-  switch (layer) {
-    case 1:
-      ergodox_right_led_1_on();
-      break;
-    case 2:
-      ergodox_right_led_2_on();
-      break;
-    case 3:
-      ergodox_right_led_3_on();
-      break;
-    case 4:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      break;
-    case 5:
-      ergodox_right_led_1_on();
-      ergodox_right_led_3_on();
-      break;
-    case 6:
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
-      break;
-    case 7:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
-      break;
-    default:
-      break;
-  }
-  return state;
-};
